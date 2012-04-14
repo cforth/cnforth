e91cdab2d82ce77d5e4fa197d839aba371ec37da
xiaohao@cforth <cforth@163.com>
2012-04-14 21:13:55 +0800

    Update colon_words.h

--- colon_words.h
+++ colon_words.h
@@ -1,16 +1,16 @@
-/********************** cforth... ************************/
+/********************** cforth扩展词 ************************/
 
-
+/* 2drop */
 void drop2(){ drop(); drop(); } 
 
-
+/* over */
 void over(){ tor(); dup(); rto(); swap(); } 
 
-
+/* 2dup  */
 void dup2(){ over(); over(); } 
 
+/* show top of DS */
 void showtopDS(){ showTop(); drop(); } 
 
-
+/* 带余数的除法 */
 void div() { dup2(); mod(); tor(); ddiv();rto(); ifFlag0(); }
-
