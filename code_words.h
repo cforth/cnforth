1c509deb45672e1897a212221c9793071df4ba9e
xiaohao@cforth <cforth@163.com>
2012-06-10 15:05:38 +0800

    更新了注释

--- code_words.h
+++ code_words.h
@@ -1,13 +1,14 @@
-/********************** cforth核心词 ************************
- * 定义数组ds与rs，申请一段空间用作数据栈和DS返回栈RS       * 
- * 定义指针*dsp与*rsp，分别指向dS数组与rS数组的首地址       */
+/**************************** code_words.h ******************************
+ * ds[256] ==> DataStack						* 
+ * rs[256] ==> ReturnStack						*
+ ************************************************************************/
 int ds[256],rs[256];
 int *dsp=ds,*rsp=rs;
 
 /* Length of DS */
 int dsLen()
 {
-    int *tmp=&ds[1];
+	int *tmp=&ds[1];
 	int n;
 	for(n=0;tmp<=dsp;n++)
 		tmp++;
@@ -30,134 +31,142 @@ void stackOver()
 	printf("Stack Overflow!\n");
 }
 
+/* 清空DS栈 */
+void clean_ds()
+{
+	dsp=&ds[0];
+	return;
+}
+ 
 /* push 将数压入数据栈DS栈顶  */
 void push(int a)
 { 
 	dsp++;
 	*dsp=a;
+	return;
 } 
 
 /* >r 将DS栈顶的数弹出，压入RS栈顶  */
-int tor()
+void tor()
 {
 	if(dsLen() < 1){
 		stackOver();
-		return 0;
+		return ;
 	} 
 	rsp++;
 	*rsp=*dsp;
 	dsp--;
-	return 0;
+	return;
 } 
 
 /* r> 将RS栈顶的数弹出，压入DS栈顶  */
-int rto()
+void rto()
 { 
 	if(rsLen() < 1){
 		stackOver();
-		return 0;
+		return;
 	} 
 	dsp++; 
 	*dsp=*rsp; 
 	rsp--; 
-	return 0;
+	return;
 } 
 
 /* drop 丢弃DS栈顶  */
-int drop()
+void drop()
 {
 	if(dsLen() < 1){
 		stackOver();
-		return 0;
+		return;
 	} 
 	dsp--; 
-	return 0;
+	return;
 } 
 
 /* dup 复制DS栈顶数  */
-int dup()
+void dup()
 {
 	if(dsLen() < 1) {
 		stackOver();
-		return 0;
+		return;
 	} 
 	dsp++; 
 	*dsp=*(dsp-1); 
-	return 0;
+	return;
 } 
 
 /* swap 交换DS栈顶前两个数  */
-int swap()
+void swap()
 {
 	if(dsLen() < 2) {
 		stackOver();
-		return 0;
+		return;
 	} 
 	int tmp; 
 	tmp=*dsp;
 	*dsp=*(dsp-1);
 	*(dsp-1)=tmp;
-	return 0;
+	return;
 } 
 
 /* + 将DS栈顶前两个数相加  */
-int add()
+void add()
 {
 	if(dsLen() < 2) {
 		stackOver();
-		return 0;
+		return;
 	} 
 	*(dsp-1)=*(dsp-1) + *dsp; 
 	dsp--; 
-	return 0;
+	return;
 } 
 
 /* - 将DS次栈顶数减DS栈顶数  */
-int sub()
+void sub()
 {
 	if(dsLen() < 2) {
 		stackOver();
-		return 0;
+		return;
 	} 
 	*(dsp-1)=*(dsp-1) - *dsp; 
 	dsp--;
-	return 0;
+	return;
 } 
 
 /* * 将DS次栈顶数乘以DS栈顶数  */
-int mul()
+void mul()
 {
 	if(dsLen() < 2) {
 		stackOver();
-		return 0;
+		return;
 	} 
 	*(dsp-1)=*(dsp-1) * *dsp; 
 	dsp--;
-	return 0;
+	return;
 } 
 
 /* /d 将DS次栈顶数除以DS栈顶数，舍弃余数  */
-int ddiv()
+void ddiv()
 {
 	if(dsLen() < 2) {
 		stackOver();
-		return 0;
+		return;
 	}
 	*(dsp-1)=*(dsp-1) / *dsp; 
 	dsp--;
-	return 0;
+	return;
 } 
 
 /* % 将DS次栈顶数除以DS栈顶数，只留余数  */
-int mod()
+void mod()
 {
 	if(dsLen() < 2) {
 		stackOver();
-		return 0;
+		return;
 	} 
 	*(dsp-1)=*(dsp-1) % *dsp; 
 	dsp--;
-	return 0;
+	return;
 } 
 
 /* .s 显示数据栈DS  */
@@ -166,10 +175,11 @@ void showDS()
 	printf("[%d] ",dsLen());
 	int *tmp=&ds[1];
 	while(tmp<=dsp) {
-	printf("%d ",*tmp);
-        tmp++;
+		printf("%d ",*tmp);
+        	tmp++;
 	}
 	printf("<DS]\n");
+	return;
 } 
 
 /* .rs 显示返回栈RS  */
@@ -178,16 +188,18 @@ void showRS()
 	printf("[%d] ",rsLen());
 	int *tmp=&rs[1];
 	while(tmp<=rsp) {
-	printf("%d ",*tmp);
-	tmp++;
+		printf("%d ",*tmp);
+		tmp++;
 	}
 	printf("<RS]\n");
+	return;
 } 
 
 /* showTop 显示DS栈顶，对DS栈无影响 */
 void showTop()
 {
 	printf("%d\n",*dsp);
+	return;
 } 
 
 /* ifFlag0 若DS栈顶数为0，则丢弃DS栈顶，用于带余数除法 */
