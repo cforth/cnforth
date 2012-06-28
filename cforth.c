e8c0fc05515c884a2af68683e68e721a42cc5a0c
xiaohao@cforth <cforth@163.com>
2012-06-28 16:49:50 +0800

    给compile_words预留了位置。

--- cforth.c
+++ cforth.c
@@ -1,8 +1,8 @@
 /**************************** cforth.c **********************************
- * Name: Cforth	0.3.3							*
+ * Name: Cforth_alpha 0.3.3						*
  * Copyright: ear & xiaohao						*
  * Author: ear & xiaohao						*
- * Date: 27-06-12 21:33							*
+ * Date: 28-06-12 16:00							*
  * Description: Cforth is a forth interpreter, using C language		*
  ************************************************************************/	 
 #include <stdio.h>
@@ -22,15 +22,14 @@ const char word_str[COREWORDS_NUM][WORD_WIDTH] =
 {	".s",	".rs",	".",	"swap",	">r",
 	"r>",	"dup",	"drop",	"2drop","2dup",
 	"over",	"+",	"-",	"*",	"/",
-	"/d",	"%",	"--",	"++",	"rot"	};		
+	"/d",	"%",	"--",	"++",	"rot"	};
+		
 typedef void (*pType) (void) ;
 pType word_pointer[COREWORDS_NUM] = 
 {	showDS,	showRS,	showTop,swap,	tor,
 	rto,	dup,	drop,	drop2,	dup2,
 	over,	add,	sub,	mul,	ddiv,
 	divv,	mod,	sub1,	add1,	rot	}; 
-
-
 /************************************************************************/ 
 
 
@@ -38,18 +37,24 @@ pType word_pointer[COREWORDS_NUM] =
  *cforth主控制结构							*/  
 int main()
 {
-	printf("Cforth 0.3.3, ear & xiaohao Copyright (C) 2008-2012 \n");
-	printf("Cforth comes with ABSOLUTELY NO WARRANTY.\n");
-	printf("Enjoy it and have a good time! Type 'bye' to exit!\n");
+	printf("Cforth_alpha 0.3.3\n"); 
 
+	char* token;
 	int status = INTERPRETER;
 	char input_buff[MAX_LENGTH];
-	while (status == INTERPRETER) { 
-		gets_input(input_buff);
+	
+	while(1) { 
+		printf(">>>");
+		gets_input(input_buff);		
+		token = strtok(input_buff, " ");
 		
-		char* token = strtok(input_buff, " ");
 		while(token != NULL) {
-        		interpret_words(token);
+			if(status == INTERPRETER)
+        			status = interpret_words(token);
+        			
+        		else if(status == COMPILER)
+        			status = compiler_words(token);
+        			
         		token = strtok(NULL, " ");
     		}
 	}	
@@ -59,10 +64,10 @@ int main()
 
 /************************************************************************
  *字符串输入接收函数，可以从键盘或者文本中接收输入字符串 		*/
-int gets_input(char *str)
+int gets_input(char* str)
 {
 	char c;
-	for(; (c=getchar())!='\n'; str++)
+	for(; (c = getchar()) != '\n'; str++)
 		*str = c;
 	*str = '\0';
 	return 0;
@@ -72,42 +77,65 @@ int gets_input(char *str)
 
 /************************************************************************
  *cforth解释器函数							*/ 
-int interpret_words(char *str)
+int interpret_words(char* str)
 {
 	int i;
+	int status = INTERPRETER;
 	for (i = 0; i <= COREWORDS_NUM; i++) {
 		if( isNum(str) ) {
 			push(atoi(str));
 			break;
 		}
-		else if(!strcmp(word_str[i],str)) {
+		
+		else if( !strcmp(word_str[i], str) ) {
 			word_pointer[i]();
 			break;
 		}
-		else if( !strcmp("bye",str) ) 
+		
+		else if( !strcmp(":", str) ) {
+			status = COMPILER;
+			break;
+		}
+		
+		else if( !strcmp("bye", str) ) 
 			exit(0);
 			
-		else if(i == COREWORDS_NUM){
-			printf("Undifine word!\n>>>%s<<<\n",str);
+		else if(i == COREWORDS_NUM) {
+			printf("Undefine word!\n-->%s<--\n",str);
 			clean_ds();
 			break;
 		}
 	}
 	
-	return 0;	
+	return status;
 }
+
 /*数值字符串判断函数，是int型则返回1，否则返回0				*/ 
-int isNum(char *str) 
+int isNum(char* str) 
 {
-	if(*str=='-' && *(str+1)=='\0')
+	if(*str == '-' && *(str+1) == '\0')
 		return 0;
-	if((*str<'0' || *str>'9') && *str!='-')
+
+	if((*str < '0' || *str > '9') && *str != '-')
 		return 0;
+
 	str++;
-	for(; *str!='\0'; str++) {
-		if(*str<'0' || *str>'9')
+	for(; *str != '\0'; str++) {
+		if(*str < '0' || *str > '9')
 			return 0;
 	}
 	return 1;
 }
+/************************************************************************/ 
+
+
+/************************************************************************
+ *cforth编译器函数(未完成)						*/ 
+int compiler_words(char* str)
+{
+	int status = COMPILER;
+	if( !strcmp(";", str) ) 
+		status = INTERPRETER;
+	return status;
+} 
 /************************************************************************/ 
