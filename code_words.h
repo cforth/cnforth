e5449811f88e031901d9532de6786821fac9b11b
xiaohao@cforth <cforth@163.com>
2012-07-01 16:01:49 +0800

    核心字头文件

--- code_words.h
+++ code_words.h
@@ -1,221 +1,167 @@
-/**************************** code_words.h ******************************
- * ds[256] ==> DataStack						* 
- * rs[256] ==> ReturnStack						*
- ************************************************************************/
-int ds[256],rs[256];
-int *dsp = ds, *rsp = rs;
+/*
+** code_words.h
+** 核心字模块的接口
+*/
 
-/* Length of DS */
-int dsLen()
-{
-	return dsp - ds;
-}
-
-/* Length of RS */
-int rsLen()
-{
-	return rsp - rs;
-}
-
-/* Warning of stack overflow, clean DS */
-void stackOver()
-{
-	dsp = ds;
-	printf("Stack underflower!\n");
-	return;
-}
-
-/* Clean contents of DS */
-void clean_ds()
-{
-	dsp = ds;
-	return;
-}
- 
-/* Push into DS */
-void push(int num)
-{ 
-	dsp++;
-	*dsp = num;
-	return;
-} 
-
-/* >r  Pop from the top of DS, push into RS */
-void tor()
-{
-	if(dsLen() < 1){
-		stackOver();
-		return ;
-	} 
-	rsp++;
-	*rsp = *dsp;
-	dsp--;
-	return;
-} 
-
-/* r>  Pop from the top of RS, push into DS */
-void rto()
-{ 
-	if(rsLen() < 1){
-		stackOver();
-		return;
-	} 
-	dsp++; 
-	*dsp = *rsp; 
-	rsp--; 
-	return;
-} 
-
-/* drop  Drop the top of DS */
-void drop()
-{
-	if(dsLen() < 1){
-		stackOver();
-		return;
-	} 
-	dsp--; 
-	return;
-} 
-
-/* dup  Dup the top of DS, push into DS */
-void dup()
-{
-	if(dsLen() < 1) {
-		stackOver();
-		return;
-	} 
-	dsp++; 
-	*dsp = *(dsp-1); 
-	return;
-} 
-
-/* swap  Swap of the first two top of DS */
-void swap()
-{
-	if(dsLen() < 2) {
-		stackOver();
-		return;
-	} 
-	int tmp; 
-	tmp = *dsp;
-	*dsp = *(dsp-1);
-	*(dsp-1) = tmp;
-	return;
-} 
-
-/* + 将DS栈顶前两个数相加  */
-void add()
-{
-	if(dsLen() < 2) {
-		stackOver();
-		return;
-	} 
-	*(dsp-1) += *dsp; 
-	dsp--; 
-	return;
-} 
-
-/* - 将DS次栈顶数减DS栈顶数  */
-void sub()
-{
-	if(dsLen() < 2) {
-		stackOver();
-		return;
-	} 
-	*(dsp-1) -= *dsp; 
-	dsp--;
-	return;
-} 
-
-/* * 将DS次栈顶数乘以DS栈顶数  */
-void mul()
-{
-	if(dsLen() < 2) {
-		stackOver();
-		return;
-	} 
-	*(dsp-1) *= *dsp; 
-	dsp--;
-	return;
-} 
-
-/* /d 将DS次栈顶数除以DS栈顶数，舍弃余数  */
-void ddiv()
-{
-	if(dsLen() < 2) {
-		stackOver();
-		return;
-	}
-	*(dsp-1) /= *dsp; 
-	dsp--;
-	return;
-} 
-
-/* % 将DS次栈顶数除以DS栈顶数，只留余数  */
-void mod()
-{
-	if(dsLen() < 2) {
-		stackOver();
-		return;
-	} 
-	*(dsp-1) %= *dsp; 
-	dsp--;
-	return;
-} 
-
-/* .s 显示数据栈DS  */
-void showDS()
-{
-	printf("[%d] ",dsLen());
-	int *tmp;
-	for(tmp=ds+1; tmp<=dsp; tmp++)
-		printf("%d ",*tmp);
-	printf("<DS]\n");
-	return;
-} 
-
-/* .rs 显示返回栈RS  */
-void showRS()
-{
-	printf("[%d] ",rsLen());
-	int *tmp;
-	for(tmp=rs+1; tmp<=rsp; tmp++)
-		printf("%d ",*tmp);
-	printf("<RS]\n");
-	return;
-} 
-
-/* . 弹出DS栈顶到显示 */
-void showTop()
-{
-	if(dsLen() < 1){
-		stackOver();
-		return ;
-	} 
-	printf("%d\n",*dsp);
-	dsp--; 
-	return;
-} 
+#define STACK_TYPE	int /*堆栈所存储的值的类型*/
 
 
-/* -- */
-void sub1()
-{
-	if(dsLen() < 1){
-		stackOver();
-		return ;
-	}
-	*dsp -= 1;
-	return;
-}
+/*
+** ds_len
+** 返回数据栈中值的数量 
+*/
+int ds_len(void);
 
-/* ++ */
-void add1()
-{
-	if(dsLen() < 1){
-		stackOver();
-		return ;
-	}
-	*dsp += 1;
-	return;
-}
+
+/*
+** rs_len
+** 返回返回栈中值的数量
+*/
+int rs_len(void);
+
+
+/*
+** stack_error
+** 清空数据栈和返回栈，并打印出警告信息
+*/
+void stack_error( void );
+
+
+/*
+** clean_stack
+** 清空数据栈和返回栈
+*/
+void clean_stack( void );
+
+
+/*
+** push
+** 把一个新值压入到数据栈中。参数是被压入的值。
+*/
+void push( STACK_TYPE value );
+
+
+/*
+** tor
+** 弹出数据栈顶值，将值压入返回栈。
+*/
+void tor( void );
+
+
+/*
+** rto
+** 弹出返回栈顶值，将值压入数据栈。
+*/
+void rto( void );
+
+
+/*
+** drop
+** 弹出数据栈顶值，并丢弃掉。
+*/
+void drop( void );
+
+
+/*
+** dup
+** 复制数据栈顶值，并压入数据栈。
+*/
+void dup( void );
+
+
+/*
+** swap
+** 交换数据栈栈顶和次栈顶位置的值。
+*/
+void swap( void );
+
+
+/*
+** add
+** 弹出数据栈栈顶和次栈顶位置的值并相加。
+** 结果压入数据栈。
+*/
+void add( void );
+
+
+/*
+** sub
+** 弹出数据栈栈顶和次栈顶位置的值。
+** 栈顶值最为减数，次栈顶值作为被减数。
+** 相减后将结果压入数据栈。
+*/
+void sub( void );
+
+
+/*
+** mul
+** 弹出数据栈栈顶和次栈顶位置的值。
+** 相乘后将结果压入数据栈。
+*/
+void mul( void );
+
+
+/*
+** div_new
+** 弹出数据栈栈顶和次栈顶位置的值。
+** 栈顶值最为除数，次栈顶值作为被除数。
+** 相除后将结果压入数据栈。
+** */
+void div_new( void );
+
+
+/*
+** mod
+** 弹出数据栈栈顶和次栈顶位置的值。
+** 栈顶值最为除数，次栈顶值作为被除数。
+** 相除后将余数压入数据栈。
+*/
+void mod( void );
+
+
+/*
+** sh_ds
+** 显示数据栈中的值，不影响数据栈。
+*/
+void sh_ds( void );
+
+
+/*
+** sh_rs
+** 显示返回栈中的值，不影响返回栈。
+*/
+void sh_rs( void );
+
+
+/*
+** pop
+** 弹出数据栈栈顶，并显示到输出。
+*/
+void pop( void );
+
+
+/*
+** sub1
+** 将数据栈栈顶中的值减1。
+*/
+void sub1( void );
+
+
+/*
+** add1
+** 将数据栈栈顶中的值加1。
+*/
+void add1( void );
+
+
+/*
+** over
+** Forth中over的效果。
+*/
+void over( void );
+
+/*
+** rot
+** Forth中rot的效果。
+*/
+void rot( void );
