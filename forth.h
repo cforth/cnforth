b0c78bec74bff17b224252fce2d60312b39b4a1c
ChaiFei <cforth@163.com>
2016-01-07 20:21:34 +0800

    重构代码

--- forth.h
+++ forth.h
@@ -1,12 +1,6 @@
 #include "words.h"
 #include "parse.h"
 
-int is_num(char *s);
-
-int find(char *name, Dict *dict);
-
-void explain();
-
 void interpret(char *s, Dict *dict);
 
 int load_file(char *file_path);
